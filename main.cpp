3d5001dd7324bce6b1da04fca7f9f0f8177a0ffa
artyom74 <artemterentev66@gmail.com>
2024-05-06 00:43:06 +0300

    Добавьте файлы проекта.

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <time.h>
+using namespace std;
+static void bubbleSort(int arr[], int size) {
+    int temp;
+    int count = 0;
+    for (int i = 0; i < size; i++) {
+        for (int k = 0; k < size; k++) {
+            cout << arr[k] << " ";
+        }        cout << endl;
+        for (int j = size - 1; j > i; j--) {
+            if (arr[j] < arr[j - 1]) {
+                count++;
+                temp = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = temp;
+            }
+        }
+        if (!count) {
+            break;
+        }
+        count = 0;
+    }
+}
+
+
+int main() {
+    setlocale(LC_ALL, "ru");
+
+    const int size = 10;
+
+    int arr[size];
+    for (int i = 0; i < size; i++) {
+        arr[i] = rand() % 100;
+    }
+    bubbleSort(arr, size);
+    return 0;
+}
